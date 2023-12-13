37198e9d3b43af5f44ecba3c954c20db3454e237
yaoshuai <yaoshuai@hortorgames.com>
2023-12-13 19:25:29 +0800

    study javascripts

--- todolist/script.js
+++ todolist/script.js
@@ -8,5 +8,21 @@ function addTask() {
         let li = document.createElement("li")
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)
+    }
+    inputBox.value = ""
+
+    listContainer.addEventListener("click", function(e) {
+        if (e.target.tagName === "LI") {
+            e.target.classList.toggle("checked")
+        } else if (e.garget.tagName ==="SPAN") {
+            e.garget.parentElement.remove();
+        }
+    }, false);
+
+    function saveData() {
+        localStorage.setItem("data", listContainer.innerHTML)
     }
 }
