4099e8b6e5ddfa6b042e499159699f7eb2282308
yaoshuai <yaoshuai@bkxgame.com>
2025-03-28 15:59:21 +0800

    Revert "一些goroutine的测试代码啊"
    This reverts commit 3f134bff5e331dbaa9e9f08f566c716b8b606506.

--- go-basics/main_test.go
+++ go-basics/main_test.go
@@ -1,12 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"github.com/panjf2000/ants/v2"
-	"sync"
-	"testing"
-	"time"
-)
+import "testing"
 
 func BenchmarkGoAdd(b *testing.B) {
 	for i := 0; i < b.N; i++ {
@@ -36,98 +30,4 @@ func BenchmarkGoAddIntSerial(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		GoAddIntSerial()
 	}
-}
-
-func TestAntFor(t *testing.T) {
-	pool, _ := ants.NewPool(3)
-	defer pool.Release()
-	for i := 0; i < 1000; i++ {
-		_ = pool.Submit(func() {
-			println(i)
-		})
-	}
-	time.Sleep(1 * time.Second)
-}
-
-func TestGoroutinePrintABAnts(t *testing.T) {
-	var wg sync.WaitGroup
-	aChan := make(chan struct{}, 1)
-	bChan := make(chan struct{}, 1)
-	cChan := make(chan struct{}, 1)
-
-	pool, _ := ants.NewPool(3)
-	defer pool.Release()
-
-	aChan <- struct{}{}
-
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		_ = pool.Submit(func() {
-			defer wg.Done()
-			<-aChan
-			printAFunc()
-			bChan <- struct{}{}
-		})
-	}
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		_ = pool.Submit(func() {
-			defer wg.Done()
-			<-bChan
-			printBFunc()
-			cChan <- struct{}{}
-		})
-	}
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		_ = pool.Submit(func() {
-			defer wg.Done()
-			<-cChan
-			printCFunc()
-			aChan <- struct{}{}
-		})
-	}
-	wg.Wait()
-	fmt.Println("finish")
-}
-
-func TestGoroutinePrintABC(t *testing.T) {
-	aChan := make(chan struct{}, 100)
-	bChan := make(chan struct{})
-	cChan := make(chan struct{})
-	aChan <- struct{}{}
-	wg := sync.WaitGroup{}
-	for i := 0; i < 10; i++ {
-		wg.Add(3)
-		go func() {
-			defer wg.Done()
-			<-aChan
-			printAFunc()
-			bChan <- struct{}{}
-		}()
-		go func() {
-			defer wg.Done()
-			<-bChan
-			printBFunc()
-			cChan <- struct{}{}
-		}()
-		go func() {
-			defer wg.Done()
-			<-cChan
-			printCFunc()
-			aChan <- struct{}{}
-		}()
-	}
-	wg.Wait()
-	//这个不想要但是提交了
-}
-
-var printAFunc = func() {
-	println("A")
-}
-var printBFunc = func() {
-	println("B")
-}
-var printCFunc = func() {
-	println("C")
 }
